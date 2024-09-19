dc1a790db7badd9da007efa9e5886b800150ed54
Papa Mamadou Mbaye <mistermbaye@MacBook-Pro-de-Papa.local>
2024-09-19 16:13:41 +0200

    finalisation erreur console

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -8,53 +8,58 @@ const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
 
-  // Tri des événements par date en ordre décroissant
-  const byDateDesc = data?.focus.slice().sort((evtA, evtB) =>
-    new Date(evtB.date) - new Date(evtA.date)
-  );
-
-  // Fonction pour passer à la carte suivante
+  const byDateDesc = data?.focus.sort((evtA, evtB) =>
+    new Date(evtA.date) > new Date(evtB.date) ? -1 : 1 // changement "<" par ">" pour ordre decroissant
+  ) ;
+ 
+ 
   const nextCard = () => {
-    if (byDateDesc) {
-      setIndex((prevIndex) => (prevIndex < byDateDesc.length - 1 ? prevIndex + 1 : 0));
-    }
+    setTimeout(
+      () => setIndex(index +1 < byDateDesc?.length ? index + 1 : 0),
+      5000
+    );
   };
 
   useEffect(() => {
-    const timer = setTimeout(nextCard, 5000);
-    return () => clearTimeout(timer); // Nettoyage du timeout à la désactivation du composant
-  }, [index, byDateDesc]); // Ajout de dépendances
+    nextCard();
+  });
 
+  
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
-        <div
-          key={event.id} // Utilisation d'un identifiant unique pour la clé
-          className={`SlideCard SlideCard--${index === idx ? "display" : "hide"}`}
-        >
-          <img src={event.cover} alt="forum" />
-          <div className="SlideCard__descriptionContainer">
-            <div className="SlideCard__description">
-              <h3>{event.title}</h3>
-              <p>{event.description}</p>
-              <div>{getMonth(new Date(event.date))}</div>
+        <div key={event.title}>
+          <div 
+            className={`SlideCard SlideCard--${
+              index === idx ? "display" : "hide"
+            }`}
+          >
+            <img src={event.cover} alt="forum" />
+            <div className="SlideCard__descriptionContainer">
+              <div className="SlideCard__description">
+                <h3>{event.title}</h3>
+                <p>{event.description}</p>
+                <div>{getMonth(new Date(event.date))}</div>
+              </div>
+            </div>
+
+            
+          </div>
+          <div className="SlideCard__paginationContainer">
+            <div className="SlideCard__pagination">
+              {byDateDesc?.map((_, radioIdx) => (
+                <input
+                  key={`${_.title}`}
+                  type="radio"
+                  name="radio-button"
+                  checked={index === radioIdx}
+                  onChange={()=>null}
+                />
+              ))}
             </div>
           </div>
         </div>
       ))}
-      <div className="SlideCard__paginationContainer">
-        <div className="SlideCard__pagination">
-          {byDateDesc?.map((event, radioIdx) => (
-            <input
-              key={event.id} // Utilisation d'un identifiant unique pour la clé
-              type="radio"
-              name="radio-button"
-              checked={index === radioIdx}
-              onChange={() => setIndex(radioIdx)}
-            />
-          ))}
-        </div>
-      </div>
     </div>
   );
 };
